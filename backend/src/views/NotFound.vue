57e75983a953b9fd29f7d9b69eb381f27277af9f
NipitponPhenkul <nipitponphenkul@hotmail.com>
2023-10-02 22:27:54 +0700

    update font page, router, components and page NotFound

---
+++ backend/src/views/NotFound.vue
@@ -0,0 +1,27 @@
+<template lang="">
+    <div class="flex justify-center items-center min-h-full w-full min-w-full">
+        <div class="flex">
+            <div class="text-indigo-600 self-start text-5xl px-8 font-bold border-r pb-8 leading-2">
+                404
+            </div>
+            <div class="px-8">
+                <h1 class="text-5xl font-bold mb-2">Page Not Found</h1>
+                <p class="text-gray-600 mb-8">Please check the URL in address bar and try again</p>
+                <div>
+                    <router-link :to="{name: 'app.dashboard'}"
+                    class="justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm 
+                    hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 
+                    focus-visible:outline-indigo-600">
+                    Go back home
+                    </router-link>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup>
+
+</script>
+<style lang="">
+    
+</style>
